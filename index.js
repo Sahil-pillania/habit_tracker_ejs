a162b51d49b3d018045c21f135844a9e322ae2a9
Sahil pillania <sahilpillania88@gmail.com>
2023-03-24 21:15:00 +0530

    main backend file

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const expressLayouts = require("express-ejs-layouts");
+
+const app = express();
+
+//-----DB Config---------//
+const db = "mongodb://127.0.0.1:27017/habit_tracker";
+
+//------Connect to Mongo--------//
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB successfully!"))
+  .catch((err) => console.log(err));
+
+//-----EJS---------//
+app.use(expressLayouts);
+app.use("/assets", express.static("./assets"));
+app.set("view engine", "ejs");
+
+//------BodyParser----------
+app.use(express.urlencoded({ extended: false }));
+
+//-----Accessing Routes--------------------------------
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, console.log(`Server started on port  ${PORT}`));
